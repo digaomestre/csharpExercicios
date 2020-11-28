361a02b4b4b61ca877e14b7c26e6879b40de76de
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-11-28 00:23:33 -0300

    Exercicio seção 3 nivel basico primeiro

--- secao3/saidaProdutos/Program.cs
+++ secao3/saidaProdutos/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-
+using System.Globalization;
 namespace saidaProdutos
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           string Produto1 = "Computador";
+           string Produto2 = "Mesa de escritório";
+
+            byte idade = 30;
+            int codigo = 5290;
+            char genero = 'M';
+
+            double preco1 = 2100.00;
+            double preco2 = 650.50;
+            double medida = 53.234567;
+
+            Console.WriteLine("Produtos:");
+            Console.WriteLine($"{Produto1}, cujo preço é {preco1}");
+            Console.WriteLine($"{Produto2}, cujo preço é {preco2}");
+            Console.WriteLine($"Registro: {idade} anos de idade, código {codigo} e gênero: {genero}");
+            Console.WriteLine($"Medida com 8 casas decimais {medida:F8}");
+            Console.WriteLine($"Arrendondado(Três casas decimais): {medida:F3}");
+            Console.WriteLine("Separador decimal invariant culture:" +medida.ToString("F3", CultureInfo.InvariantCulture));
+
         }
     }
 }
