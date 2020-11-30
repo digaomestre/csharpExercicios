6585e607ec72e1c83edd9e41f02df1de62f2bf88
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-11-30 19:11:17 -0300

    Finalizacao do programa de entrada de dados.

--- secao3/entradaDadosUsu/Program.cs
+++ secao3/entradaDadosUsu/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace entradaDadosUsu
 {
@@ -6,7 +7,26 @@ namespace entradaDadosUsu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Entre com seu nome completo:");
+            string nomeCompleto = Console.ReadLine();
+            Console.WriteLine("Quantos quartos tem na sua casa:");
+            int qtde = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o preço do produto:");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("entre seu ultimo nome, idade e altura (mesma linha)");
+            string[] dados = Console.ReadLine().Split(' ');
+            string sobrenome = dados[0];
+            int idade = int.Parse(dados[1]);
+            double altura = double.Parse(dados[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nomeCompleto);
+            Console.WriteLine(qtde);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(sobrenome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura);
+
         }
     }
 }
