4b3bf3066eb55e0b65bb7aeab16d265300b3aa26
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-01 18:14:08 -0300

    Finalizado o exercicio de formas

--- secao3/exerciciosSequenciais/formas/Program.cs
+++ secao3/exerciciosSequenciais/formas/Program.cs
@@ -13,14 +13,19 @@ namespace formas
             Console.WriteLine("Digite o valor de C: ");
             double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double triangulo = a*c;
+            double triangulo = a*c/2;
             double circulo = Math.PI*Math.Pow(c, 2);
             double trapezio = (a+b)*c/2;
             double quadrado = Math.Pow(b, 2);
             double retangulo = a*b;
 
-            Console.WriteLine("TRIANGULO : "+triangulo.ToString("F4", CultureInfo.InvariantCulture));
-            Console.WriteLine("Circulo : "+Circulo.ToString("F4", CultureInfo.InvariantCulture));
+
+            Console.WriteLine("TRIANGULO : "+triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO : "+circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO : "+ trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO : "+ quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO : "+ retangulo.ToString("F3", CultureInfo.InvariantCulture));
         }
     }
 
+}
