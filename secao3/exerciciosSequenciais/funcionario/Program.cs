b92fe9d81ac047f314d40d8b3a27e30dd65b3656
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-11-30 22:21:00 -0300

    Criado exercicio calcular salario do funcionario

---
+++ secao3/exerciciosSequenciais/funcionario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace funcionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
