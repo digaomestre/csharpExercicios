6ea25cd89851a6c2fd8782ca213ffa4c53f26d10
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-01 10:14:39 -0300

    Finalizado o exercicio venda de peças

--- secao3/exerciciosSequenciais/pecas/Program.cs
+++ secao3/exerciciosSequenciais/pecas/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-
+using System.Globalization;
 namespace pecas
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Codigo Peça:");
+            int codigo1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade: ");
+            int qtde1 = int.Parse(Console.ReadLine());
+            double valor1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Codigo Peça:");
+            int codigo2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Quantidade: ");
+            int qtde2 = int.Parse(Console.ReadLine());
+            double valor2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = (valor1*qtde1)+(valor2*qtde2);
+
+            Console.WriteLine("Valor a pagar: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
         }
     }
 }
