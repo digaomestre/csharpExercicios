2895b1d6c5b54af0357278c58965ade4fecb96e6
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-01 11:54:09 -0300

    Correçã para mostrar mensagem de digitação do valor da peça.

--- secao3/exerciciosSequenciais/pecas/Program.cs
+++ secao3/exerciciosSequenciais/pecas/Program.cs
@@ -10,12 +10,14 @@ namespace pecas
             int codigo1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Quantidade: ");
             int qtde1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor da peça: ");
             double valor1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Console.WriteLine("Codigo Peça:");
             int codigo2 = int.Parse(Console.ReadLine());
             Console.WriteLine("Quantidade: ");
             int qtde2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor da peça: ");
             double valor2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             double total = (valor1*qtde1)+(valor2*qtde2);
