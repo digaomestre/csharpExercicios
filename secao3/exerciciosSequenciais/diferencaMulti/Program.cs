00e95ad8d90f8e5119916d2fe244e98dac1373de
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-11-30 21:53:06 -0300

    Criado exercicio diferença do produto de a e b por c e d

---
+++ secao3/exerciciosSequenciais/diferencaMulti/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace diferencaMulti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
