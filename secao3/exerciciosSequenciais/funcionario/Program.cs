6469cf7d0233086ffee7af6ddb8e9f88bfd5fa5b
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-01 09:32:59 -0300

    Finalizado o exercicio de calculo salario funcinario.

--- secao3/exerciciosSequenciais/funcionario/Program.cs
+++ secao3/exerciciosSequenciais/funcionario/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace funcionario
 {
@@ -6,7 +7,18 @@ namespace funcionario
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite o código do funcionário:");
+            int codFunc = int.Parse(Console.ReadLine());
+            Console.WriteLine("Horas trabalhadas: ");
+            int horasTrab = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor da hora:");
+            double valHoras = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salario = horasTrab*valHoras;
+
+            Console.WriteLine($"Codigo: {codFunc}");
+            Console.WriteLine("Salário: R$ "+salario.ToString("F2", CultureInfo.InvariantCulture));
+
+
         }
     }
 }
