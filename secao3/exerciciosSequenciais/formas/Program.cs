8d245c34d3fe5e29c9caf98d31fda1a236af1bec
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-01 17:46:09 -0300

    Realizando as seções de calculo do script

--- secao3/exerciciosSequenciais/formas/Program.cs
+++ secao3/exerciciosSequenciais/formas/Program.cs
@@ -13,7 +13,14 @@ namespace formas
             Console.WriteLine("Digite o valor de C: ");
             double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            
+            double triangulo = a*c;
+            double circulo = Math.PI*Math.Pow(c, 2);
+            double trapezio = (a+b)*c/2;
+            double quadrado = Math.Pow(b, 2);
+            double retangulo = a*b;
+
+            Console.WriteLine("TRIANGULO : "+triangulo.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Circulo : "+Circulo.ToString("F4", CultureInfo.InvariantCulture));
         }
     }
-}
+
