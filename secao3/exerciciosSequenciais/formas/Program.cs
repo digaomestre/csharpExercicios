6adb160c90e3eb04a2bf6d4ee0dd405695a2876a
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-01 12:26:05 -0300

    Criado exercicio calcular formas

---
+++ secao3/exerciciosSequenciais/formas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace formas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor de A: ");
+            double a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de B: ");
+            double b = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Digite o valor de C: ");
+            double c = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            
+        }
+    }
+}
