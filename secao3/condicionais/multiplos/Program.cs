d9744ee6455408c65b31b566146851354428afc1
Rodrigo Tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-04 12:12:03 -0300

    Criado exercicio de verifica multiplos.

---
+++ secao3/condicionais/multiplos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
