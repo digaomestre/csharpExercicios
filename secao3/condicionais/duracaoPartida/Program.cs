6d4f4ecd3c7ea40e517f478b0cd75c0e222dde74
rodrigo tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-08 09:18:24 -0300

    criado exercicio duracao jogo

---
+++ secao3/condicionais/duracaoPartida/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace duracaoPartida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
