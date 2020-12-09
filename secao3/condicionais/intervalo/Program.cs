1e88fe90c74db020086754fd9f935b65b70107c5
rodrigo tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-09 19:16:24 -0300

    criado o exercicio verifica intervalo.

---
+++ secao3/condicionais/intervalo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace intervalo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
