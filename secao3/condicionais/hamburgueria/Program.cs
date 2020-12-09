42af2d6cbf7bd4522a65276d5ab5cd931afe6790
rodrigo tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-09 17:49:59 -0300

    criado menu para o exercicio hamburgueria

--- secao3/condicionais/hamburgueria/Program.cs
+++ secao3/condicionais/hamburgueria/Program.cs
@@ -6,7 +6,14 @@ namespace hamburgueria
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bem vindos a humburgueir do papai!");
+            Console.WriteLine("Menu - Opções: ");
+            Console.WriteLine("1 - Cachorro Quente R$ 4.00");
+            Console.WriteLine("2 - X - Salada R$ 4.50");
+            Console.WriteLine("3 - X - Frango R$ 5.00");
+            Console.WriteLine("4 - Torrada Simples R$ 2.00");
+            Console.WriteLine("5 - Refrigerante R$ 1.00");
+
         }
     }
 }
