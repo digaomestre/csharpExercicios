a5c5206db7cd8bda741d7788ce682393b9af98e5
rodrigo tomaz <rodrigo_souza2013@yahoo.com.br>
2020-12-09 17:19:28 -0300

    finalizado exercicio duração jogo.

--- secao3/condicionais/duracaoPartida/Program.cs
+++ secao3/condicionais/duracaoPartida/Program.cs
@@ -9,6 +9,16 @@ namespace duracaoPartida
             String[] hora = Console.ReadLine().Split(' ');
             int horaInicial = int.Parse(hora[0]);
             int horaFinal = int.Parse(hora[1]);
+            int duracao = 0;
+            Console.WriteLine($"Inicial: {horaInicial}");
+            Console.WriteLine($"Final: {horaFinal}");
+            if(horaInicial < horaFinal){
+                duracao = horaFinal - horaInicial;
+            }else{
+                duracao = (24 - (horaFinal + horaInicial)); 
+            }
+
+            Console.WriteLine($"Jogou durante:{duracao}");
         }
     }
 }
