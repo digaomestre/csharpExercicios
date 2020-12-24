5e73d10e3f1be7c7433354fa65475447253fa3d1
Rodrigo Souza <rodrigo_souza2013@yahoo.com.br>
2020-12-24 15:26:03 -0300

    criado exercicio verifica senha com o comando while.

--- secao3/condicionais/intervalo/Program.cs
+++ secao3/condicionais/intervalo/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
-
+using System.Globalization;
 namespace intervalo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("*******Verifica interlo.*********");
+            Console.WriteLine("Digite o valor:");
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            if(valor > 0 && valor <= 25){
+                Console.WriteLine("[0 - 25]");
+            }else if(valor <= 50){
+                Console.WriteLine("[25 - 50]");
+            }else if(valor <= 75){
+                Console.WriteLine("[50 - 75]");
+            }else if(valor <= 100){
+                Console.WriteLine("[75 - 100]");
+            }else{
+                Console.WriteLine("Fora de intervalo");
+            }
         }
     }
 }
---
+++ secao3/condicionais/quadrante/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace quadrante
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
