fe49ce5edc89927760996a4dec932746eb18dfeb
Rodrigo Souza <rodrigo_souza2013@yahoo.com.br>
2020-12-25 00:03:07 -0300

    Finalizado exercicio quadrante em while.

--- secao3/repeticoes/while/quadrante/Program.cs
+++ secao3/repeticoes/while/quadrante/Program.cs
@@ -6,7 +6,27 @@ namespace quadrante
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite as coordenadas (x, y)");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            //int x = int.Parse(Console.ReadLine());
+            //int y = int.Parse(Console.ReadLine());
+            while (x != 0 && y != 0)
+            {
+                if(x > 0 && y > 0){
+                    Console.WriteLine("Primeiro");
+                }else if(x < 0 && y >0){
+                    Console.WriteLine("Segundo");
+                }else if(x < 0 && y < 0){
+                    Console.WriteLine("Terceiro");
+                }else{
+                    Console.WriteLine("Quarto");
+                }
+                Console.WriteLine("Digite as coordenadas(x, y)");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+            }
+
         }
     }
 }
