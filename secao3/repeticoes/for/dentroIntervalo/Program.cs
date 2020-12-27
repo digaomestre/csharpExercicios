e5dde0d9aaaf410a84e327ed0d2b132f0c77c1b9
Rodrigo Souza <rodrigo_souza2013@yahoo.com.br>
2020-12-27 14:36:27 -0300

    criado exercicio calcular media ponderada.

---
+++ secao3/repeticoes/for/dentroIntervalo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace dentroIntervalo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c_in = 0, c_out = 0;
+            Console.WriteLine("Digite a quantidade numeros:");
+            int qtde = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= qtde; i++)
+            {
+                System.Console.WriteLine($"Digite o {i}º valores:");
+                int val = int.Parse(Console.ReadLine());
+                if(val > 9 && val < 21){
+                    c_in++;
+                }else
+                {
+                    c_out++;
+                }
+            }
+            System.Console.WriteLine($"{c_in} in \n{c_out} out");
+        }
+    }
+}
