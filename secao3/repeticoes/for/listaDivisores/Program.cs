aafdc5efcf6a46f30ed4052ba34848cd2693dccc
Rodrigo Souza <rodrigo_souza2013@yahoo.com.br>
2020-12-28 15:19:28 -0300

    finalizado exercicio divisores.

---
+++ secao3/repeticoes/for/listaDivisores/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace listaDivisores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um numero inteiro");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if(n%i == 0){
+                    Console.WriteLine(i);
+                }
+            }
+
+           
+        }
+    }
+}
