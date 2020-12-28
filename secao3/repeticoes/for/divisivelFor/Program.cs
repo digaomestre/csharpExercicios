af0d77af854827898dcda0e3e69ec59074d4db8c
Rodrigo Souza <rodrigo_souza2013@yahoo.com.br>
2020-12-28 11:35:17 -0300

    criado exercicio fatorial.

---
+++ secao3/repeticoes/for/divisivelFor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace divisivelFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade:");
+            int qtde = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Digite os valores:");
+            for (int i = 0; i < qtde; i++)
+            {
+                string[] v = Console.ReadLine().Split(' ');
+                double v1 = double.Parse(v[0], CultureInfo.InvariantCulture);
+                double v2 = double.Parse(v[1], CultureInfo.InvariantCulture);
+
+                if(v2 == 0){
+                    System.Console.WriteLine("Divisão impossivel");
+                }else
+                {
+                    double total = v1/v2;
+                    System.Console.WriteLine(total.ToString("F1", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
