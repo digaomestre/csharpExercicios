d42e57c18d5fdfe875b4b94f3aa06a19410cfbd5
Rodrigo Souza <rodrigo_souza2013@yahoo.com.br>
2020-12-31 14:11:03 -0300

    Finalizado exercicio verfica mais velho com POO

---
+++ secao3/POO/verificaMaisVelho/Pessoas.cs
@@ -0,0 +1,8 @@
+namespace verificaMaisVelho
+{
+    public class Pessoas
+    {
+        public string nome;
+        public int idade;
+    }
+}
--- secao3/POO/verificaMaisVelho/Program.cs
+++ secao3/POO/verificaMaisVelho/Program.cs
@@ -6,7 +6,25 @@ namespace verificaMaisVelho
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Pessoas p1 = new Pessoas();
+            Pessoas p2 = new Pessoas();
+            System.Console.WriteLine("Digite o nome e a idade da primeiro pessoa:");
+            string[] d1 = Console.ReadLine().Split(' ');
+            p1.nome = d1[0];
+            p1.idade = int.Parse(d1[1]);
+           System.Console.WriteLine("Digite o nome e a idade da segunda pessoa");
+	    string[] d2 = Console.ReadLine().Split(' ');
+            p2.nome = d2[0];
+            p2.idade = int.Parse(d2[1]);
+
+            if(p1.idade > p2.idade){
+                Console.WriteLine($"{p1.nome} é mais velho");
+            }else if(p1.idade < p2.idade){
+                Console.WriteLine($"{p2.nome} é mais velho");
+            }else{
+                Console.WriteLine($"{p1.nome} e {p2.nome} tem a mesma idade");
+            }
+            
         }
     }
 }
