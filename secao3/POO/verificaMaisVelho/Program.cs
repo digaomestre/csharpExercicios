7920bf138c1c7de1267be8696b5f4afc82459fbf
Rodrigo Souza <rodrigo_souza2013@yahoo.com.br>
2020-12-30 23:16:52 -0300

    criado exercicio verifica mais velho em POO.

---
+++ secao3/POO/verificaMaisVelho/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace verificaMaisVelho
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
